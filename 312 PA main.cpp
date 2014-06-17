706ba5723f95c062746a70c5922f070c6bac9993
jcranek <jcranek@neo.tamu.edu>
2014-06-16 23:31:01 -0500

    prt 1 and 2

--- 312 PA main.cpp
+++
@@ -1,8 +0,0 @@
-// Joshua Cranek
-// 312 Prog Assig
-
-#include <stdexcept>
-#include <iostream>
-
-using namespace std;
-
---
+++ 312 PA prt 2.h
@@ -0,0 +1,47 @@
+// Joshua Cranek
+// 312 Prog Assig
+
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+//part 1
+
+static int functionCount;
+
+bool And(bool a, bool b){
+	
+	functionCount++;
+	return a && b;
+	
+}
+
+
+bool Or (bool a, bool b){
+	
+	functionCount++;
+	return a||b;
+	
+}
+
+	
+bool Not(bool a){
+	
+	functionCount++;
+	return !a;
+	
+}
+
+
+bool XOR(bool a, bool b)
+bool Mux(bool operation, bool output_if_true, bool output_if_false)
+bool Sum(bool a, bool b) 
+bool Sum(bool a bool b, bool c)
+bool CarryOut(bool a, bool b, bool c)
+bool Equal(bool a, bool b)
+
+
+void printFunctionCount()
+void print(bool a)
+void resetFunctionCount()
