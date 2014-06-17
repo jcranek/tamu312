aded0e9e927ba018f776834d885fc5cdd4988c06
jcranek <jcranek@neo.tamu.edu>
2014-06-16 23:37:25 -0500

    3.0

---
+++ 312 PA prt 3.cpp
@@ -0,0 +1,27 @@
+// Joshua Cranek
+// 312 Prog Assig
+
+#include <stdexcept>
+#include <iostream>
+
+using namespace std;
+
+//part 3
+
+static int functionCount;
+
+
+Create variables for each of the following:
+Let 16 (00010000)= ADDU
+Let 17 (00010001)= SUBU
+Let 2 (00000010)= NEG
+Let 3 (00000011)= EQ
+Let 4 (00000100)= LT
+Let 5 (00000101)= GT
+Let 6 (00000110)= AND
+Let 7 (00000111)= OR
+Let 8 (00001000)= SLL
+Let 9 (00001001)= SRL
+
+Create an 8 bit ALU that can do all of the above operations on a byte:
+bool* alu(bool op_code[8], bool* output, bool input1[8], bool input2[8]);
