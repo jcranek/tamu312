486255af03b93f88ac72c87e54ec9dc59d3c1b7b
jcranek <jcranek@neo.tamu.edu>
2014-06-23 00:46:52 -0500

    6/23

--- 312 PA prt 3.cpp
+++ 312 PA prt 3.cpp
@@ -29,41 +29,6 @@ static int functionCount;
 
 bool* alu(bool op_code[8], bool* output, bool input1[8], bool input2[8]){
 
-	switch(BinToDec(op_code))
-	{
-		case OP_ADDU:
-			return addu(output,input1,input2);
-			break;
-		case OP_SUBU:
-			return subu(output,input1,input2);
-			break;
-		case OP_NEG:
-			return negate(output,input1);
-			break;
-		case OP_EQ:
-			return equal(output,input1,input2);
-			break;
-		case OP_LT:
-			return lessthan(output,input1,input2);
-			break;
-		case OP_GT:
-			return greaterthan(output,input1,input2);
-			break;
-		case OP_AND:
-			return and8(output,input1,input2);
-			break;
-		case OP_OR:
-			return or8(output,input1,input2);
-			break;
-		case OP_SLL:
-			return shiftLeft(output,input1);
-			break;
-		case OP_SRL:
-			return shiftRight(output,input1);
-			break;
-		default:
-			return zero;
-			break;
-	}
+	
 
 }
